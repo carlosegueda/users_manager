1788def6302fe39e39941961efef95d608f20ce4
Carlos Osegueda <carlosegueda200@gmail.com>
2025-06-30 16:35:03 -0600

    Mejora en form login

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -3,40 +3,55 @@ import "../css/Login.css";
 export default function LoginPage() {
   return (
     <>
-    
       <div class="login">
-        <div class="form-login"><form>
+        <div class="form-login">
+          <form>
             <h1>Users Manager</h1>
-          <div class="input-group">
-            <label for="user">Usuario:</label>
-            <input
-              id="user"
-              name="user"
-              autocomplete="off"
-              type="text"
-              required
-            />
-          </div>
-
-          <div class="input-group">
-            <label for="password">Contraseña:</label>
-            <input
-              id="password"
-              name="password"
-              autocomplete="off"
-              type="password"
-              required
-            />
-          </div>
-            <span class="forgot"><a href="#">Has Olvidado tu contraseña?</a></span>
-          <button class="submit" type="submit">Iniciar Sesión</button>
-        </form></div>
-        
-        <div class="info-login">
-            <div class="info">
-                <h1>¿Qué es Users Manager?</h1>
-                <h2>Aplicación web desarrollada con React, HTML, CSS, JavaScript, NodeJS, Express y SQL que permite la gestión de usuarios y los permisos respectivos en el Dashboard.</h2>
+            <div class="input-group">
+              <label for="user">Usuario:</label>
+              <input
+              class="userinput"
+                id="user"
+                name="user"
+                autocomplete="off"
+                type="text"
+                required
+              />
             </div>
+
+            <div class="input-group">
+              <label for="password">Contraseña:</label>
+              <input
+                class="passwordinput"
+                id="password"
+                name="password"
+                autocomplete="off"
+                type="password"
+                required
+              />
+            </div>
+            <span class="forgot">
+              <a href="#">Has Olvidado tu contraseña?</a>
+            </span>
+            <button class="submit" type="submit">
+              Iniciar Sesión
+            </button>
+            <span class="registrar">No tienes cuenta?   
+              <a href="/register">Regístrate</a>
+            </span>
+            <a href="https://github.com/carlosegueda" target="_blank"><img src="/github-logo.png" class="giticon"/></a>
+          </form>
+        </div>
+
+        <div class="info-login">
+          <div class="info">
+            <h1>¿Qué es Users Manager?</h1>
+            <h2>
+              Aplicación web desarrollada con React, HTML, CSS, JavaScript,
+              NodeJS, Express y SQL que permite la gestión de usuarios y los
+              permisos respectivos en el Dashboard.
+            </h2>
+          </div>
         </div>
       </div>
     </>
